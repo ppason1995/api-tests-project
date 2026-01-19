0288b09f4c1ff76f9377f8868581656f522c047c
Paweł Pasoń <ppason1995@gmail.com>
2026-01-19 22:22:33 +0100

    add contract-based negative tests for create user

--- tests/users/test_create_user.py
+++ tests/users/test_create_user.py
@@ -1,6 +1,6 @@
 from tests.helpers.schema_validators import validate_user_schema
 
-def test_create_user(users_client):
+def test_create_user_success(users_client):
     payload={
         "name" : "Test User",
         "email" : "test.user@example.com"
@@ -15,3 +15,25 @@ def test_create_user(users_client):
 
     assert data["name"] == payload["name"]
     assert data["email"] == payload["email"]
+
+def test_create_user_missing_email(users_client):
+    payload = {
+        "name" : "Test User"
+    }
+
+    response = users_client.create_user(payload)
+    assert response.status_code in (200, 201)
+
+    data = response.json()
+
+    # response does not fulfill full user contract
+    assert "email" not in data
+
+def test_create_user_with_empty_payload(users_client):
+    response = users_client.create_user({})
+    assert response.status_code in (200, 201)
+
+    data = response.json()
+
+    assert "name" not in data
+    assert "email" not in data
